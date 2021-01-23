ce89f7c6e06a4a861609e499192b9e1153c2a0ac
Bingcheng Ni <nibingcheng@johndeere.com>
2021-01-23 15:35:06 -0600

    bonus #2 implemented bug exists

--- script1.js
+++ script1.js
@@ -1,16 +1,16 @@
 // console.log("We are live")
 let dakar = ['Port of Spain', 'Gitega', 'Dakar', 'Nouakchott'];
-let freetown = ['Sao Tome', 'Freetown', 'Nicosia', 'Niamey'];
+let freetown = ['Sao Tome', 'Freetown', 'Nicosia', 'Ulaanbaatar'];
 let banjul = ['Banjul', 'Ankara', 'Rabat', 'Victoria'];
-let vientiane = ['Vientiane', 'Kuala Lumpur', 'Manama', 'Kuwait City'];
+let vientiane = ['Vientiane', 'Kuala Lumpur', 'Hanoi', 'Kuwait City'];
 let yangon = ['Copenhagen', 'Castries', 'Yangon', 'Kingston'];
 let kualaLumpur = ['Vaduz', 'Jakarta', 'Moscow', 'Kuala Lumpur'];
 let stockholm = ['Paris', 'Stockholm', 'Tbilisi', 'Prague'];
-let montevideo = ['Montevideo', 'Panama City', 'Caracas', 'Peking'];
-let kigali = ['Kigali', 'Porto Novo', 'San Tome', 'Panama City'];
-let sanMarino = ['Bucharest', 'San Marino', 'Manila', 'Podgorica'];
-let suva = ['Ngerulmud', 'Panama City', 'Suva', 'Palikir'];
-let kiev = ['Sarajevo', 'Taipei', 'Kiev', 'Skopje'];
+let montevideo = ['Montevideo', 'Lima', 'Caracas', 'Peking'];
+let kigali = ['Kigali', 'Porto Novo', 'Ottawa', 'Panama City'];
+let sanMarino = ['Bucharest', 'San Marino', 'Manila', 'Turin'];
+let suva = ['Ngerulmud', 'Penang', 'Suva', 'Palikir'];
+let kiev = ['Sarajevo', 'Taipei', 'Kiev', 'Budapest'];
 let valletta = ['Rome', 'Riga', 'Tokyo', 'Valletta'];
 let bangkok = ['Male', 'Kuwait City', 'Rabat', 'Bangkok'];
 let dhaka = ['Kabul', 'Singapore', 'Pyongyang', 'Dhaka'];
@@ -57,92 +57,64 @@ function  randomQuizSequence() {
 let pageNum =0;  //Page number index [starting from 0]
 let points = 0;  //each correct answer earns 10 points
 let answerClicked = false;
+let str = sessionStorage.getItem('myStorage');
+
+if (str != null) {
+    pageNum = parseInt(str.split(",")[0]);
+    points = parseInt(str.split(',')[1]);
+}
 
 let score = document.querySelector('.second'); 
 let tracking = document.querySelector('.third');
 let question = document.querySelector('.fourth');
-let choice1 = document.querySelector('.fifth');
-let choice2 = document.querySelector('.sixth');
-let choice3 = document.querySelector('.seventh');
-let choice4 = document.querySelector('.eighth');
 let next = document.querySelector('p');
 let selection = document.querySelector('#lower');
 
+let choice = [];
+for (let i=0; i<4; i++) {
+    choice[i] = document.querySelector(`.${selection.children[i].classList[0]}`);
+}
+score.textContent = `Score: ${points}`;
 function loadNextQuestion(page) {
     tracking.textContent = ` ${page+1}/10`;  
     question.textContent = `What is the capital of ${array[page][0]}?`;
-    choice1.textContent = array[page][2][0];
-    choice2.textContent = array[page][2][1];
-    choice3.textContent = array[page][2][2];
-    choice4.textContent = array[page][2][3];   
+    
+    for (let i=0; i<4; i++) {
+        choice[i].textContent = array[page][2][i];
+    }
     reset();     
 }
-
 
 shuffleChoice();
 randomQuizSequence();
 loadNextQuestion(pageNum);
 
 selection.addEventListener('click', function(e) {
-    // console.log(e.target.attributes[0].value);
-    // allow only one click for each question
+    sessionStorage.setItem('myStorage', [pageNum, points]);   
+    // allow only one click for each question    
+    // console.log(e.target.attributes[0].value);   
     if (answerClicked == false) {    
         answerClicked = true;
         
-    // check if the clicked is a winner    
-        if (e.target.attributes[0].value == 'fifth cell') { 
-            if (choice1.textContent == array[pageNum][1]) {
-                points += 10;
-                score.textContent = `Score: ${points}`;
-                e.target.classList.add('green');
-                console.log('Jackpot!');
+        // check if the clicked is a winner      
+        // console.log(e.target.attributes[0].value);
+        // console.log(`${selection.children[1].classList}`);
+        for (let j=0; j<4; j++) {
+            if (e.target.attributes[0].value == `${selection.children[j].classList}`) { 
+                if (choice[j].textContent == array[pageNum][1]) {
+                    points += 10;
+                    score.textContent = `Score: ${points}`;
+                    e.target.classList.add('green');
+                    console.log('Jackpot!');
+                }
+                else {
+                    e.target.classList.add('red');
+                    console.log('Wrong answer!');       
+                }            
             }
-            else {
-                e.target.classList.add('red');
-                console.log('Wrong answer!');       
-            }            
         }
-
-        if (e.target.attributes[0].value == 'sixth cell') {
-            if (choice2.textContent == array[pageNum][1]) {
-                points += 10;
-                score.textContent = `Score: ${points}`;
-                e.target.classList.add('green');
-                console.log('Jackpot!');
-            }
-            else {
-                e.target.classList.add('red');
-                console.log('Wrong answer!');       
-            }            
-        }
-
-        if (e.target.attributes[0].value == 'seventh cell') {
-            if (choice3.textContent == array[pageNum][1]) {
-                points += 10;
-                score.textContent = `Score: ${points}`;
-                e.target.classList.add('green');
-                console.log('Jackpot!');
-            }
-            else {
-                e.target.classList.add('red');
-                console.log('Wrong answer!');       
-            }            
-        }
-
-        if (e.target.attributes[0].value == 'eighth cell') {
-            if (choice4.textContent == array[pageNum][1]) {
-                points += 10;
-                score.textContent = `Score: ${points}`;
-                e.target.classList.add('green');
-                console.log('Jackpot!');
-            }
-            else {
-                e.target.classList.add('red');
-                console.log('Wrong answer!');       
-            }                 
-        }
-        
-        if(pageNum === 9) {
+        // handle the final question of the quiz, click behavior and time based scoring
+        if (pageNum === 9) {
             next.textContent = 'END';
             let finishTime = new Date();
             time = (finishTime.getTime() - startTime.getTime())/1000;
@@ -153,28 +125,23 @@ selection.addEventListener('click', function(e) {
                 }
             }
             alert(`Time used: ${Math.floor(time/60)} min ${Math.floor(time)%60} sec \nYour score: ${Math.floor(points)}`);
+            // sessionStorage.removeItem('myStorage');
         }
     }      
     else {
         console.log('Illegal click!');
     }    
-
 })
-
+// When a new question loads, clear all text color from previous question's answer
 function reset() {
-    choice1.classList.remove('green');
-    choice1.classList.remove('red');
-    choice2.classList.remove('green');
-    choice2.classList.remove('red');
-    choice3.classList.remove('green');
-    choice3.classList.remove('red');
-    choice4.classList.remove('green');
-    choice4.classList.remove('red');
+    for (let k=0; k<4; k++) {
+        choice[k].classList.remove('green');
+        choice[k].classList.remove('red');
+    }
     answerClicked = false;
 }
-
-next.addEventListener('click', function(e) {
-           
+// Handle the click when user wants to go to next question 
+next.addEventListener('click', function(e) {           
     if (answerClicked == false) {
         alert('Please select your answer')
     }
@@ -184,7 +151,7 @@ next.addEventListener('click', function(e) {
         // reset();
     }
     else {
-        // console.log(pageNum);
         next.textContent = 'END';
-    }    
+    }
+    sessionStorage.setItem('myStorage', [pageNum, points]);    
 })
